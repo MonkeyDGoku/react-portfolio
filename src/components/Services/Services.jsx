c188f690bbb84b5ac7671260883896d7865430e7
Anup Mahato <anupmahato033@gmail.com>
2022-05-17 00:51:14 +0530

    first commit

---
+++ src/components/Services/Services.jsx
@@ -0,0 +1,88 @@
+import React, { useContext } from "react";
+import "./Services.css";
+import Card from "../Card/Card";
+import HeartEmoji from "../../img/heartemoji.png";
+import Glasses from "../../img/glasses.png";
+import Humble from "../../img/humble.png";
+import { themeContext } from "../../Context";
+import { motion } from "framer-motion";
+import Resume from "./resume.pdf";
+
+const Services = () => {
+  // context
+  const theme = useContext(themeContext);
+  const darkMode = theme.state.darkMode;
+
+  // transition
+  const transition = {
+    duration: 1,
+    type: "spring",
+  };
+
+  return (
+    <div className="services" id="services">
+      {/* left side */}
+      <div className="awesome">
+        {/* dark mode */}
+        <span style={{ color: darkMode ? "white" : "" }}>My Awesome</span>
+        <span>services</span>
+        <span>
+          I build web apps in React.Js, Vue.Js with Bootstrap, Material Ui,
+          Vuetify, Tailwind Css.
+          <br />I develop microservices, rest apis and console apps using
+          ASP.NET
+        </span>
+        <a href={Resume} download>
+          <button className="button s-button">Download CV</button>
+        </a>
+        <div className="blur s-blur1" style={{ background: "#ABF1FF94" }}></div>
+      </div>
+      {/* right */}
+      <div className="cards">
+        {/* first card */}
+        <motion.div
+          initial={{ left: "25rem" }}
+          whileInView={{ left: "14rem" }}
+          transition={transition}
+        >
+          <Card
+            emoji={HeartEmoji}
+            heading={"Backend"}
+            detail={"C#, ASP.NET, Web Api, CSOM"}
+          />
+        </motion.div>
+        {/* second card */}
+        <motion.div
+          initial={{ left: "-11rem", top: "12rem" }}
+          whileInView={{ left: "-4rem" }}
+          transition={transition}
+        >
+          <Card
+            emoji={Glasses}
+            heading={"Developer"}
+            detail={"Html, Css, JavaScript, React.Js, Vue.Js,Nodejs, Express"}
+          />
+        </motion.div>
+        {/* 3rd */}
+        <motion.div
+          initial={{ top: "19rem", left: "25rem" }}
+          whileInView={{ left: "12rem" }}
+          transition={transition}
+        >
+          <Card
+            emoji={Humble}
+            heading={"Database"}
+            detail={"Sql, Pl/Sql, Sql server, Mongodb, Oracle"}
+            color="rgba(252, 166, 31, 0.45)"
+          />
+        </motion.div>
+        <div
+          className="blur s-blur2"
+          style={{ background: "var(--purple)" }}
+        ></div>
+      </div>
+    </div>
+  );
+};
+
+export default Services;
